8f0b36ce26116e977cdda2ce91719661e779c6be
defenxe <yong020504@gmail.com>
2025-09-14 21:41:44 +0900

    Update transforms.py

--- nuscenes/cross_view_transformer/data/transforms.py
+++ nuscenes/cross_view_transformer/data/transforms.py
@@ -1,4 +1,4 @@
-#수정된 파일
+'''
 
 import pathlib
 
@@ -200,3 +200,224 @@ class LoadDataTransform(torchvision.transforms.ToTensor):
 
         return result
 
+'''
+
+
+
+#수정된 파일
+
+import pathlib
+
+import torch
+import torchvision
+import numpy as np
+
+from PIL import Image
+from .common import encode, decode
+from .augmentations import StrongAug, GeometricAug
+
+
+class Sample(dict):
+    def __init__(
+        self,
+        token,
+        scene,
+        intrinsics,
+        extrinsics,
+        images,
+        view,
+        bev,
+        object_count=0,
+        attention_entropy=None, # --- 수정/추가된 부분 ---
+        **kwargs
+    ):
+        super().__init__(**kwargs)
+
+        # Used to create path in save/load
+        self.token = token
+        self.scene = scene
+
+        self.view = view
+        self.bev = bev
+
+        self.images = images
+        self.intrinsics = intrinsics
+        self.extrinsics = extrinsics
+
+        # 새로 추가된 속성들
+        self.object_count = object_count
+        self.attention_entropy = attention_entropy # --- 수정/추가된 부분 ---
+
+    def __getattr__(self, key):
+        return super().__getitem__(key)
+
+    def __setattr__(self, key, val):
+        self[key] = val
+
+        return super().__setattr__(key, val)
+
+
+class SaveDataTransform:
+    """
+    All data to be saved to .json must be passed in as native Python lists
+    """
+    def __init__(self, labels_dir):
+        self.labels_dir = pathlib.Path(labels_dir)
+
+    def get_cameras(self, batch: Sample):
+        return {
+            'images': batch.images,
+            'intrinsics': batch.intrinsics,
+            'extrinsics': batch.extrinsics
+        }
+
+    def get_bev(self, batch: Sample):
+        result = {
+            'view': batch.view,
+        }
+
+        scene_dir = self.labels_dir / batch.scene
+
+        bev_path = f'bev_{batch.token}.png'
+        Image.fromarray(encode(batch.bev)).save(scene_dir / bev_path)
+
+        result['bev'] = bev_path
+
+        # Auxilliary labels
+        if batch.get('aux') is not None:
+            aux_path = f'aux_{batch.token}.npz'
+            np.savez_compressed(scene_dir / aux_path, aux=batch.aux)
+
+            result['aux'] = aux_path
+
+        # Visibility mask
+        if batch.get('visibility') is not None:
+            visibility_path = f'visibility_{batch.token}.png'
+            Image.fromarray(batch.visibility).save(scene_dir / visibility_path)
+
+            result['visibility'] = visibility_path
+
+        # Save object_count
+        if batch.object_count is not None:
+            result['object_count'] = batch.object_count
+
+        # --- 수정/추가된 부분 ---
+        # Save attention_entropy
+        if batch.attention_entropy is not None:
+            # torch.Tensor를 JSON에 저장하기 위해 list로 변환
+            result['attention_entropy'] = batch.attention_entropy.tolist()
+        # ----------------------
+
+        return result
+
+    def __call__(self, batch):
+        """
+        Save sensor/label data and return any additional info to be saved to json
+        """
+        result = {}
+        result.update(self.get_cameras(batch))
+        result.update(self.get_bev(batch))
+        result.update({k: v for k, v in batch.items() if k not in result})
+
+        return result
+
+
+class LoadDataTransform(torchvision.transforms.ToTensor):
+    def __init__(self, dataset_dir, labels_dir, image_config, num_classes, augment='none'):
+        super().__init__()
+
+        self.dataset_dir = pathlib.Path(dataset_dir)
+        self.labels_dir = pathlib.Path(labels_dir)
+        self.image_config = image_config
+        self.num_classes = num_classes
+
+        xform = {
+            'none': [],
+            'strong': [StrongAug()],
+            'geometric': [StrongAug(), GeometricAug()],
+        }[augment] + [torchvision.transforms.ToTensor()]
+
+        self.img_transform = torchvision.transforms.Compose(xform)
+        self.to_tensor = super().__call__
+
+    def get_cameras(self, sample: Sample, h, w, top_crop):
+        """
+        Note: we invert I and E here for convenience.
+        """
+        images = list()
+        intrinsics = list()
+
+        for image_path, I_original in zip(sample.images, sample.intrinsics):
+            h_resize = h + top_crop
+            w_resize = w
+
+            image = Image.open(self.dataset_dir / image_path)
+
+            image_new = image.resize((w_resize, h_resize), resample=Image.BILINEar)
+            image_new = image_new.crop((0, top_crop, image_new.width, image_new.height))
+
+            I = np.float32(I_original)
+            I[0, 0] *= w_resize / image.width
+            I[0, 2] *= w_resize / image.width
+            I[1, 1] *= h_resize / image.height
+            I[1, 2] *= h_resize / image.height
+            I[1, 2] -= top_crop
+
+            images.append(self.img_transform(image_new))
+            intrinsics.append(torch.tensor(I))
+
+        return {
+            'cam_idx': torch.LongTensor(sample.cam_ids),
+            'image': torch.stack(images, 0),
+            'intrinsics': torch.stack(intrinsics, 0),
+            'extrinsics': torch.tensor(np.float32(sample.extrinsics)),
+        }
+
+    def get_bev(self, sample: Sample):
+        scene_dir = self.labels_dir / sample.scene
+        bev = None
+
+        if sample.bev is not None:
+            bev = Image.open(scene_dir / sample.bev)
+            bev = decode(bev, self.num_classes)
+            bev = (255 * bev).astype(np.uint8)
+            bev = self.to_tensor(bev)
+
+        result = {
+            'bev': bev,
+            'view': torch.tensor(sample.view),
+        }
+
+        if 'visibility' in sample:
+            visibility = Image.open(scene_dir / sample.visibility)
+            result['visibility'] = np.array(visibility, dtype=np.uint8)
+
+        if 'aux' in sample:
+            aux = np.load(scene_dir / sample.aux)['aux']
+            result['center'] = self.to_tensor(aux[..., 1])
+
+        if 'pose' in sample:
+            result['pose'] = np.float32(sample['pose'])
+
+        # Load object_count
+        if 'object_count' in sample:
+            result['object_count'] = sample['object_count']
+
+        # --- 수정/추가된 부분 ---
+        # Load attention_entropy
+        if 'attention_entropy' in sample:
+            # JSON에서 list로 읽은 값을 tensor로 변환
+            result['attention_entropy'] = torch.tensor(sample['attention_entropy'])
+        # ----------------------
+
+        return result
+
+    def __call__(self, batch):
+        if not isinstance(batch, Sample):
+            batch = Sample(**batch)
+
+        result = dict()
+        result.update(self.get_cameras(batch, **self.image_config))
+        result.update(self.get_bev(batch))
+
+        return result
